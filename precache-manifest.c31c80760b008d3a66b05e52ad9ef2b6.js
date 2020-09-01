cc1110e64f4e3f2310bc8f61e6049f70282f46da
Ajay Sharma <ajay.sharma@polestarllp.com>
2020-09-01 14:47:36 +0530

    Updates

---
+++ precache-manifest.c31c80760b008d3a66b05e52ad9ef2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8016e6f6e317631e03591806e5d701b9",
+    "url": "/CurrencyConverter/index.html"
+  },
+  {
+    "revision": "bc47e7d9f7a14a0f8dff",
+    "url": "/CurrencyConverter/static/css/main.73a5f67f.chunk.css"
+  },
+  {
+    "revision": "5949759f8399c0cd61ff",
+    "url": "/CurrencyConverter/static/js/2.634dada2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CurrencyConverter/static/js/2.634dada2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc47e7d9f7a14a0f8dff",
+    "url": "/CurrencyConverter/static/js/main.c1d8c8de.chunk.js"
+  },
+  {
+    "revision": "eea822b2adefa6ab59ef",
+    "url": "/CurrencyConverter/static/js/runtime-main.133c845f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/CurrencyConverter/precache-manifest.c31c80760b008d3a66b05e52ad9ef2b6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CurrencyConverter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
